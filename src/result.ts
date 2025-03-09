d9af4cd91edf8eabfe631aa286212725f3123659
Carl Holwell <carl.holwell@gmail.com>
2025-03-09 22:47:27 +0000

    think its ok

--- src/result.ts
+++ src/result.ts
@@ -1,17 +1,20 @@
-export type Result<V = undefined, E = undefined> = Ok<V> | Error<E>;
+export type Result<V, E> = Ok<V> | Error<E>;
 
 // Ok
 
-export type Ok<V = undefined> = {
+export type Ok<V> = {
     type: "ok";
     value: V;
 };
 
-export function ok<V>(value: V): Ok<V> {
+export function ok<V>(value: V): Ok<V>;
+export function ok(): Ok<None>;
+
+export function ok<V>(value?: V): Ok<V> {
     return {
         type: "ok",
-        value: value
-    }
+        value: value as V
+    };
 }
 
 export function isOk<V>(result: Result<V, unknown>): result is Ok<V> {
@@ -25,13 +28,20 @@ export type Error<E> = {
     error: E;
 };
 
-export function error<E>(error: E): Error<E> {
+export function error<E>(error: E): Error<E>;
+export function error(): Error<None>;
+
+export function error<E>(error?: E): Error<E> {
     return {
         type: "error",
-        error: error
-    }
+        error: error as E
+    };
 }
 
 export function isError<E>(result: Result<unknown, E>): result is Error<E> {
     return result.type === "error";
 }
+
+// None
+
+export type None = undefined;
