6a8a4af1afb436bfee3dab0a098e060032a88d9a
Carl Holwell <carl.holwell@gmail.com>
2025-03-22 18:45:32 +0000

    nice

--- src/result.ts
+++ src/result.ts
@@ -1,32 +1,39 @@
+// Result
+
 export type Result<V = undefined, E = undefined> = Ok<V> | Error<E>;
 
 // Ok
 
-export type Ok<V = undefined> = {
-    type: "ok";
-    value: V;
-};
+export class Ok<V = undefined> {
+    readonly type = "ok";
+    constructor(public value: V) {}
+
+    isOk(): this is Ok<V> {
+        return true;
+    }
+
+    isError(): this is Error<never> {
+        return false;
+    }
+}
 
 export function ok<V>(value: V): Ok<V>;
 export function ok(): Ok;
 
 export function ok<V>(value?: V): Ok<V> {
-    return {
-        type: "ok",
-        value: value as V
-    };
+    return new Ok(value as V);
 }
 
 export function isOk<V>(result: Result<V, unknown>): result is Ok<V> {
-    return result.type === "ok";
+    return result.isOk();
 }
 
 export function okOrDefault<V>(result: Result<V, unknown>, defaultValue: V): V {
-    return isOk(result) ? result.value : defaultValue;
+    return result.isOk() ? result.value : defaultValue;
 }
 
 export function okOrThrow<V>(result: Result<V, unknown>): V {
-    if (isError(result)) {
+    if (result.isError()) {
         throw new Error("result is error");
     }
     return result.value;
@@ -34,31 +41,36 @@ export function okOrThrow<V>(result: Result<V, unknown>): V {
 
 // Error
 
-export type Error<E = undefined> = {
-    type: "error";
-    error: E;
-};
+export class Error<E = undefined> {
+    readonly type = "error";
+    constructor(public error: E) {}
+
+    isOk(): this is Ok<never> {
+        return false;
+    }
+
+    isError(): this is Error<E> {
+        return true;
+    }
+}
 
 export function error<E>(error: E): Error<E>;
 export function error(): Error<undefined>;
 
 export function error<E>(error?: E): Error<E> {
-    return {
-        type: "error",
-        error: error as E
-    };
+    return new Error(error as E);
 }
 
 export function isError<E>(result: Result<unknown, E>): result is Error<E> {
-    return result.type === "error";
+    return result.isError();
 }
 
-export function errorOrDefalt<E>(result: Result<unknown, E>, defaultError: E): E {
-    return isError(result) ? result.error : defaultError;
+export function errorOrDefault<E>(result: Result<unknown, E>, defaultError: E): E {
+    return result.isError() ? result.error : defaultError;
 }
 
 export function errorOrThrow<E>(result: Result<unknown, E>): E {
-    if (isOk(result)) {
+    if (result.isOk()) {
         throw new Error("result is ok");
     }
     return result.error;
